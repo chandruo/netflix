32e8057d62ea96570c54fa74dfd1535ce74b60bd
siva <chandru04042000@gmail.com>
2026-03-12 20:13:04 +0530

    open ai added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,13 +1,14 @@
 export const LOGO =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-02-12/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-
- export const  API_OPTIONS  = {
-  method: 'GET',
+export const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY
+export const API_OPTIONS = {
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTk4NWI3ZGIyZWYwMTFkZjVkMmQ5YjhmNGIyODdmMSIsIm5iZiI6MTc3MjY0OTc4NS44OTMsInN1YiI6IjY5YTg3ZDM5OTBhMTVkMDg1MjA4MGI0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kKNyJVku11vuNiiH7-BYnSw9KLl126nKzUj4aNnV4go'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTk4NWI3ZGIyZWYwMTFkZjVkMmQ5YjhmNGIyODdmMSIsIm5iZiI6MTc3MjY0OTc4NS44OTMsInN1YiI6IjY5YTg3ZDM5OTBhMTVkMDg1MjA4MGI0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kKNyJVku11vuNiiH7-BYnSw9KLl126nKzUj4aNnV4go",
+  },
 };
 
-export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
